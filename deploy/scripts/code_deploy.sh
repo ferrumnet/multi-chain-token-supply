620adae17e29ce75cfc3036e454d1a5f1f8220d3
Sheraz Riaz <sheraz>
2022-06-15 08:30:49 +0500

    CI/CD enabled

---
+++ deploy/scripts/code_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /root/set_env.sh
+
+export REPO=chain-circulating-supply-dev
+docker stop ${AppName}-${Environment}-chain || true
+docker system prune -a -f || true
+aws ecr get-login-password --region ${Region} | docker login --username AWS --password-stdin ${AccountId}.dkr.ecr.${Region}.amazonaws.com/${REPO}
+docker run -d -it --name ${AppName}-${Environment}-chain  -p 80:8080 --restart unless-stopped ${AccountId}.dkr.ecr.${Region}.amazonaws.com/${REPO}:latest
